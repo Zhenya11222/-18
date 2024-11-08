1542f66bce193c033a9d5023a46728ae65ccf1fc
Zhenya11222 <derkaczena81@gmail.com>
2024-11-08 20:22:34 +0200

    Initial commit

---
+++ my_django_project/settings_patch.py
@@ -0,0 +1,12 @@
+
+# Database settings for PostgreSQL in Docker
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
